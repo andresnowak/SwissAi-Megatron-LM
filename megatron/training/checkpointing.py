69af5f5c43df83fa88ddd483dc1e96402c4f120b
Andres Nowak <andresnowak52@gmail.com>
2025-12-20 17:54:47 +0100

    Added num_zero_experts and zero_expert_aux_loss_tau to checkpointing loading of argument from checkpoint

--- megatron/training/checkpointing.py
+++ megatron/training/checkpointing.py
@@ -1299,6 +1299,8 @@ def load_args_from_checkpoint(
     _set_arg('hybrid_mlp_ratio', force=True)
 
     _set_arg('num_experts', force=True)
+    _set_arg('num_moe_zero_experts', force=True)
+    _set_arg('moe_zero_expert_aux_loss_tau', force=True)
     _set_arg('moe_layer_freq', force=True)
     if getattr(checkpoint_args, 'num_experts', None) is not None:
         _set_arg('moe_ffn_hidden_size', force=True)
